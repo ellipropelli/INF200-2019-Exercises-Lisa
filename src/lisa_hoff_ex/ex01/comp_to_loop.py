647ff1ac76c49e1b5abf617afc5ad594751edbb0
Lisa Hoff <lisast@nmbu.no>
2019-09-20 11:55:50 +0200

    Did small changes.

--- src/lisa_hoff_ex/ex01/comp_to_loop.py
+++ src/lisa_hoff_ex/ex01/comp_to_loop.py
@@ -1,8 +1,9 @@
 def squares_by_loop(n):
     square = []
-    for k in range(1, 14):
+    for k in range(n):
         if k % 3 == 1:
             square.append(k**2)
     return square
+print (squares_by_loop(10))
 
 
