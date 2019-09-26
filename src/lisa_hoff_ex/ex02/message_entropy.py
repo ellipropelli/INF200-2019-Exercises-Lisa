81510e327ea6a2b5e59e16b3c54b2df93abd325d
Lisa Hoff <lisast@nmbu.no>
2019-09-26 13:00:25 +0200

    Create file message_entropy.py and added the letter_freqs() function from Excercise 01 from the sample solution. Added the following code.

--- src/lisa_hoff_ex/ex02/message_entropy.py
+++ src/lisa_hoff_ex/ex02/message_entropy.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-#__author__ = "Lisa Hof"
-#__email__ = "yngve.m.moe@gmail.com"
+#__author__ = "Lisa Hoff"
+#__email__ = "lisahoffstr@gmail.com"
 
 from collections import Counter, defaultdict
 
@@ -9,3 +9,12 @@ from collections import Counter, defaultdict
 def letter_freq(txt):
     # Best solution
     return Counter(txt.lower())
+
+
+def entropy(message):
+    pass
+
+
+if __name__ == "__main__":
+    for msg in '', 'aaaa', 'aaba', 'abcd', 'This is a short text.':
+        print('{:25}: {:8.3f} bits'.format(msg, entropy(msg)))
