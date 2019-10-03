a64612f1524e973b79703c0b3761d2e922ae9adc
Lisa Hoff <lisast@nmbu.no>
2019-10-03 20:38:30 +0200

    Pasted the median function from the task B

---
+++ src/lisa_hoff_ex/ex03_project/test_median.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Lisa Hoff'
+__email__ = 'lisast@nmbu.no'
+
+import pytest
+
+
+def median(data):
+    """
+    Returns median of data.
+
+    :param data: An iterable of containing numbers
+    :return: Median of data
+    """
+
+    sdata = sorted(data)
+    n = len(sdata)
+    return (sdata[n//2] if n % 2 == 1
+        else 0.5 * (sdata[n//2 - 1] + sdata[n//2]))
